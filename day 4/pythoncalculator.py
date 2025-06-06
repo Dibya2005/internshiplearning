214ce817db74190f8934ddb02fdcfe216fef3de3
Dibyajyoti Ray Choudhury <141916458+Dibya2005@users.noreply.github.com>
2025-06-06 13:06:09 +0530

    Update pythoncalculator.py

--- day 4/pythoncalculator.py
+++ day 4/pythoncalculator.py
@@ -3,7 +3,7 @@ num1=float(input("enter the 1st num"))
 num2=float(input("enter the 2nd num"))
 if op=="+":
   r=num1+num2
-  print(round(r,3))
+  print(round(r,3)) #round(r,3) round the reasult upto 3 decimal place
 elif op=="-":
   r=num1-num2
   print(round(r,3))
